7cd97672c76fca9e399d8604be8855e7f579a2f6
Daprojects1 <93226161+Daprojects1@users.noreply.github.com>
2022-01-15 20:37:28 +0000

    added comments and changed innerHtml to textcontent

--- script.js
+++ script.js
@@ -5,25 +5,31 @@ let mainBtn = document.querySelector(".mainbtn")
 let firstTRow = document.querySelector(".none")
 let table = document.querySelector("table")
 
+// adding the different input types into an array to iterate over it in main func
 let newRow = [itemName, date, amount];
 
 
 const addExpense = (e)=> {
+    // removing initial message
     e.preventDefault();
     firstTRow.remove();
+    // checks value length to see whether to run function.
       if (itemName.value.length > 0 && date.value.length > 0 && amount.value.length > 0){
         let newTrow = document.createElement("tr")
+        //  function below iterates through each input type.
         newRow.forEach((item)=>{
+        // creates a table data cell for each and appends it to created table row above.
           let newTdc = document.createElement("td")
           newTrow.appendChild(newTdc)
-          newTdc.innerHTML= item.value;
+        // adds 
+          newTdc.textContent= item.value;
           if (item === newRow[2]){
               newTdc.classList.add("relative");
-            newTdc.innerHTML = "£"+ " "+item.value
+            newTdc.textContent = "£"+ " "+item.value
             let newbtn = document.createElement("button")
             newbtn.classList.add("color")
             newbtn.classList.add("newBtn")
-            newbtn.innerHTML= "X";
+            newbtn.textContent= "X";
             newTdc.appendChild(newbtn)
             newbtn.addEventListener("click", (e)=> {
                  e.preventDefault();  
