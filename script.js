57110b8c7d5026ee0a5fe156399c0042c8e1bbd3
Daprojects1 <93226161+Daprojects1@users.noreply.github.com>
2022-01-15 20:16:33 +0000

    complete working project

--- script.js
+++ script.js
@@ -7,23 +7,35 @@ let table = document.querySelector("table")
 
 let newRow = [itemName, date, amount];
 
+
 const addExpense = (e)=> {
     e.preventDefault();
     firstTRow.remove();
-    let newTrow = document.createElement("tr")
-    newRow.forEach((item)=>{
-      let newTdc = document.createElement("td")
-      newTrow.appendChild(newTdc)
-      newTdc.innerHTML= item.value;
-      if (item === newRow[2]){
-        newTdc.innerHTML = "£"+ " "+item.value
-        let newbtn = document.createElement("button")
-        newbtn.classList.add("color")
-        newbtn.innerHTML= "X";
-        newTdc.appendChild(newbtn)
-   }
-      table.appendChild(newTrow)
-    })
+      if (itemName.value.length > 0 && date.value.length > 0 && amount.value.length > 0){
+        let newTrow = document.createElement("tr")
+        newRow.forEach((item)=>{
+          let newTdc = document.createElement("td")
+          newTrow.appendChild(newTdc)
+          newTdc.innerHTML= item.value;
+          if (item === newRow[2]){
+              newTdc.classList.add("relative");
+            newTdc.innerHTML = "£"+ " "+item.value
+            let newbtn = document.createElement("button")
+            newbtn.classList.add("color")
+            newbtn.classList.add("newBtn")
+            newbtn.innerHTML= "X";
+            newTdc.appendChild(newbtn)
+            newbtn.addEventListener("click", (e)=> {
+                 e.preventDefault();  
+                newTrow.remove();
+            })
+            table.appendChild(newTrow)
+       }
+        })
+      }
+      itemName.value= "";
+      date.value="";
+      amount.value="";
 }
 
 mainBtn.addEventListener("click", addExpense)
