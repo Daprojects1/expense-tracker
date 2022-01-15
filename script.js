70e54172339d856abf3061f3f28fb99471732b25
Daprojects1 <93226161+Daprojects1@users.noreply.github.com>
2022-01-15 20:37:57 +0000

    quick changes

--- script.js
+++ script.js
@@ -21,24 +21,29 @@ const addExpense = (e)=> {
         // creates a table data cell for each and appends it to created table row above.
           let newTdc = document.createElement("td")
           newTrow.appendChild(newTdc)
-        // adds 
+        // adds text content to each data cell
           newTdc.textContent= item.value;
+        //  last row adds a £ sign and so this conditional statemnt does a check. Also adds button to this last element. 
           if (item === newRow[2]){
               newTdc.classList.add("relative");
             newTdc.textContent = "£"+ " "+item.value
             let newbtn = document.createElement("button")
+            // adding css styles and text content for button. 
             newbtn.classList.add("color")
             newbtn.classList.add("newBtn")
             newbtn.textContent= "X";
             newTdc.appendChild(newbtn)
+            // button delete function
             newbtn.addEventListener("click", (e)=> {
                  e.preventDefault();  
                 newTrow.remove();
             })
+            // appending to table
             table.appendChild(newTrow)
        }
         })
       }
+    //  values reset after function is run.
       itemName.value= "";
       date.value="";
       amount.value="";
