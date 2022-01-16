59bc3147b540bacd592f3fe1ab16b238e206234a
Daprojects1 <93226161+Daprojects1@users.noreply.github.com>
2022-01-16 16:48:41 +0000

    refactored code

--- script.js
+++ script.js
@@ -8,45 +8,53 @@ let table = document.querySelector("table")
 // adding the different input types into an array to iterate over it in main func
 let newRow = [itemName, date, amount];
 
+let reset = ()=> {
+  itemName.value= "";
+  date.value="";
+  amount.value="";
+}
+
+let mainFunc = (arr)=> {
+  //  checks value length to see whether to run function.
+  if (itemName.value.length > 0 && date.value.length > 0 && amount.value.length > 0){
+    let newTrow = document.createElement("tr")
+    //  function below iterates through each input type.
+    arr.forEach((item)=>{
+    // creates a table data cell for each and appends it to created table row above.
+      let newTdc = document.createElement("td")
+      newTrow.appendChild(newTdc)
+    // adds text content to each data cell
+      newTdc.textContent= item.value;
+    //  last row adds a £ sign and so this conditional statemnt does a check. Also adds button to this last element. 
+      if (item === arr[2]){
+          newTdc.classList.add("relative");
+        newTdc.textContent = "£"+ " "+item.value
+        let newbtn = document.createElement("button")
+        // adding css styles and text content for button. 
+        newbtn.classList.add("color")
+        newbtn.classList.add("newBtn")
+        newbtn.textContent= "X";
+        newTdc.appendChild(newbtn)
+        // button delete function
+        newbtn.addEventListener("click", (e)=> {
+             e.preventDefault();  
+            newTrow.remove();
+        })
+        // appending to table
+        table.appendChild(newTrow)
+   }
+    })
+  }
+}
 
 const addExpense = (e)=> {
     // removing initial message
     e.preventDefault();
     firstTRow.remove();
-    // checks value length to see whether to run function.
-      if (itemName.value.length > 0 && date.value.length > 0 && amount.value.length > 0){
-        let newTrow = document.createElement("tr")
-        //  function below iterates through each input type.
-        newRow.forEach((item)=>{
-        // creates a table data cell for each and appends it to created table row above.
-          let newTdc = document.createElement("td")
-          newTrow.appendChild(newTdc)
-        // adds text content to each data cell
-          newTdc.textContent= item.value;
-        //  last row adds a £ sign and so this conditional statemnt does a check. Also adds button to this last element. 
-          if (item === newRow[2]){
-              newTdc.classList.add("relative");
-            newTdc.textContent = "£"+ " "+item.value
-            let newbtn = document.createElement("button")
-            // adding css styles and text content for button. 
-            newbtn.classList.add("color")
-            newbtn.classList.add("newBtn")
-            newbtn.textContent= "X";
-            newTdc.appendChild(newbtn)
-            // button delete function
-            newbtn.addEventListener("click", (e)=> {
-                 e.preventDefault();  
-                newTrow.remove();
-            })
-            // appending to table
-            table.appendChild(newTrow)
-       }
-        })
-      }
+   // function that creates new elements.  
+     mainFunc(newRow);
     //  values reset after function is run.
-      itemName.value= "";
-      date.value="";
-      amount.value="";
+    reset();
 }
 
 mainBtn.addEventListener("click", addExpense)
